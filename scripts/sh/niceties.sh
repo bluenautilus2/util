d65da600dadb1816799b2b365e23ac543264c963
bstevens <Beth.Stevens@infor.com>
2016-09-28 13:48:49 -0500

    niceties

---
+++ scripts/sh/niceties.sh
@@ -0,0 +1,12 @@
+PATH=./:/usr/java/jdk1.7.0_45/bin:$PATH
+
+alias vim='vi';
+alias palogs='cd /usr/local/web/peopleanswers.com/logs';
+alias httplogs='cd /etc/httpd/logs';
+alias palogs2='cd /home/jboss/jboss/standalone/log'; 
+
+alias jdeploy='/home/jboss/bin/deploy /home/jboss/deployments/pa-ear-master.ear';
+alias tdeploy='/home/tomcat/bin/deploy pa /home/tomcat/deployments/pa.war';
+alias wsdeploy='/home/tomcat/bin/deploy ws /home/tomcat/deployments/ws.war';
+alias wssdeploy='/home/tomcat/bin/deploy ws-secure /home/tomcat/deployments/ws-secure.war';
+
